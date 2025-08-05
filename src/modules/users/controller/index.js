d93426cd4898f22c0a88df154de09baf0baadbcb
parham fallah <parhamf7@gmail.com>
2025-08-06 01:11:27 +0330

    implement get user controller

--- src/modules/users/controller/index.js
+++ src/modules/users/controller/index.js
@@ -1,3 +1,5 @@
+import { eq } from 'drizzle-orm';
+
 import { db } from '../../../core/database/connection.js';
 import { users } from '../../../core/database/schema.js';
 
@@ -10,8 +12,19 @@ export const createUserController = async (req, res) => {
     res.send({ message: 'User created successfully', insertResult });
 }
 
-export const getUserController = (req, res) => {
-    res.send({ message: `Get user by id: ${req.params.userId}` });
+export const getUserController = async (req, res) => {
+    const userId = req.params.userId;
+    
+    const user = await db
+        .select({userId: users.id, email: users.email, createdAt: users.createdAt})
+        .from(users)
+        .where(eq(users.id, userId));
+    
+    if (user.length < 1) {
+        res.status(404).send({ message: 'User not found' });
+    } else {
+        res.send(user[0]);
+    }
 }
 
 export const updateUserController = (req, res) => {
