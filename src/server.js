77bf74f4302ec905df4d37c746cb009c4032af3c
parham fallah <parhamf7@gmail.com>
2025-09-14 18:47:50 +0330

    add logger middleware

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 
 import { config } from './core/config/index.js';
 import { authMiddleware } from './core/middleware/auth.js';
+import { logger } from './core/middleware/logger.js';
 
 import { usersRouter } from './modules/users/router/index.js';
 import { authRouter } from './modules/auth/router/index.js';
@@ -18,6 +19,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(logger);
 
 // Defining routes
 app.use('/auth', authRouter);
